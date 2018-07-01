d79ee683a69d33adebf8ca17560cddc49a3e2e81
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2018-06-30 20:20:34 -0700

    Start on report and rename variables.

--- demos/policy_gradient/code/policy_gradient.py
+++ demos/policy_gradient/code/policy_gradient.py
@@ -1,13 +1,22 @@
 # File: policy_gradient.py 
 # Author(s): Rishikesh Vaishnav
 # Created: 29/06/2018
+
 import logging
 import os
 import numpy as np;
+import matplotlib.pyplot as plt;
+from labellines import labelLine, labelLines;
 
 import gym
 
-# Monte Carlo Policy Gradient agent
+# - graph parameters -
+plt.rc('text', usetex=True);
+
+ALPHA_COLOR_MULT = 0.3;
+# - 
+
+# Policy agent with various utility functions for use in emulation/training.
 class MCPG_agent(object):
     def __init__(self, action_space):
         self.action_space = action_space;
@@ -16,7 +25,7 @@ class MCPG_agent(object):
     def act(self, state, theta):
         return self.select_action(state, theta);
 
-    # Selects the an action using the given paramerized policy and current
+    # Selects an action using the given paramerized policy and current
     # state.
     def select_action(self, state, theta):
         _, probs = self.action_probs(state, theta);
@@ -28,7 +37,7 @@ class MCPG_agent(object):
     def vector(self, state, action):
         # set vector to be sets of states, with one set
         # activated, depending on the action
-        vector = np.zeros((state.size * self.num_actions,)).astype(float);
+        vector = np.zeros((state.size * self.num_actions,));
         active_start_ind = action * state.size;
         active_end_ind = active_start_ind + state.size;
         vector[active_start_ind:active_end_ind] = state;
@@ -57,7 +66,6 @@ class MCPG_agent(object):
         # get softmax action probabilities
         action_probs = (map_vals_exp) / np.sum(map_vals_exp);
 
-        # TODO
         return map_vals_exp, action_probs;
 
     # Gets the gradient of the ln of the probability, evaluated at the current
@@ -86,28 +94,29 @@ class MCPG_agent(object):
 env = gym.make('CartPole-v0')
 agent = MCPG_agent(env.action_space)
 
-max_episodes = 100;
-max_steps = 200;
+MAX_EPISODES = 400;
+RUNS = 10;
+MAX_STEPS = 200;
 
-reward = 0
-done = False
-theta = np.random.rand(env.observation_space.shape[0] * env.action_space.n);
-
-# TODO adjust
 # training parameters
 gamma = 1.0;
-alphas = [0.01 * n for n in range(1, 31)];
+alphas = [0.001, 0.01, 0.1];
+num_alphas = 2;
 # -
 
-# mapping from alpha to episode length sequence
-alpha_to_ep_lengths = {};
+# Performs a training run using the given learning rate, returning an ordered 
+# list of episode lengths.
+def MCPG_training_run(alpha):
+    # sequence of episode lengths for this run
+    ep_lengths = np.zeros((MAX_EPISODES,));
 
-for alpha in alphas:
-    # sequence of episode lengths
-    ep_lengths = [];
+    reward = 0;
+    done = False;
+    theta = np.zeros((env.observation_space.shape[0] \
+            * env.action_space.n,));
 
     # continue training until cutoff
-    for _ in range(max_episodes):
+    for ep_i in range(MAX_EPISODES):
         # - run episode -
         state = env.reset()
 
@@ -120,9 +129,9 @@ for alpha in alphas:
         # list of actions
         A = [];
 
-        for t in range(max_steps):
+        for t in range(MAX_STEPS):
             # optional - render episodes
-            env.render();
+            # env.render();
 
             action = agent.act(state, theta);
 
@@ -141,21 +150,59 @@ for alpha in alphas:
         # total time of episode
         T = t + 1;
 
-        ep_lengths.append(T);
+        ep_lengths[ep_i] += T;
 
-        #print("Episode length: " + str(T));
         # -
 
         # - update parameters -
-        for t in range(T):
+        # to store reward from this time to the end
+        G = 0;
+
+        for t in reversed(range(T)):
             # get reward from this time to the end
-            G = 0;
-            discount_factor = 1;
-            for r in R[t:]:
-                G += discount_factor * r;
-                discount_factor *= gamma;
+            G = (gamma * G) + R[t];
 
             theta += alpha * G * agent.grad_ln_probs(S[t], A[t], theta);
         # -
 
-    alpha_to_ep_lengths[alpha] = ep_lengths;
+    return ep_lengths;
+
+
+# mapping from alpha to episode length sequence
+alpha_to_ep_lengths = {};
+
+for alpha in alphas:
+    print("Alpha: " + str(alpha));
+
+    # sequence of episode lengths, totaled over all RUNS
+    ep_lengths_tot = np.zeros((MAX_EPISODES,));
+
+    for run_i in range(RUNS):
+        print("\trun: " + str(run_i + 1) + "/" + str(RUNS));
+
+        ep_lengths_tot = ep_lengths_tot + MCPG_training_run(alpha);
+
+    alpha_to_ep_lengths[alpha] = ep_lengths_tot / RUNS;
+
+alpha_i = 0;
+for alpha in alphas:
+    ep_lengths = alpha_to_ep_lengths[alpha];
+    x = np.array(range(1, len(ep_lengths) + 1)).astype(float);
+    y = ep_lengths;
+    #color = (np.random.rand(), np.random.rand(), np.random.rand());
+    color = (alpha_i * ALPHA_COLOR_MULT, alpha_i * ALPHA_COLOR_MULT, \
+            alpha_i * ALPHA_COLOR_MULT);
+    plt.plot(x, y, linestyle='-', color=color, label="$\\alpha =$ " \
+            + str(alpha));
+
+    alpha_i += 1;
+
+plt.ylabel("Average Episode Length (" + str(RUNS) + " Runs)");
+plt.xlabel("Episode");
+plt.title("Monte Carlo Policy Gradient Results");
+
+labelLines(plt.gca().get_lines(),zorder=2.5)
+
+plt.savefig("MCPG_agent.pgf");
+
+plt.show();
