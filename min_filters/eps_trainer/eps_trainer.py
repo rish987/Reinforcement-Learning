Start with double-epsilon implementation and fix eps_trainer to be mean- and sign-invariant.

min_filters/eps_trainer/eps_trainer.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import numpy as np
 from torch.distributions import normal
 def get_x_lim_mult(eps_mult):
-    return ((mean ** 2 + mean_old ** 2) + (2 * (std ** 2) * torch.log(1 +\
+    return ((mean ** 2 - mean_old ** 2) + (2 * (std ** 2) * torch.log(1 +\
             eps_mult))) / (2 * (mean - mean_old))
 
 def get_x_lim(eps, up):
@@ -17,18 +17,31 @@ def get_x_lim(eps, up):
 def get_discrepancy_and_penalty_contributions(eps_down, eps_up):
     x_up = get_x_lim(eps_up, True)
     x_down = get_x_lim(eps_down, False)
+    #print((eps_up).item())
+    #print((torch.exp(dist.log_prob(x_up) - dist_old.log_prob(x_up))).item())
 
     int_1 = dist_old.get_prob_between(x_down, x_up)
     int_2 = dist.get_prob_between(x_down, x_up)
     int_3 = dist_old.get_prob_above(x_up)
     int_4 = dist.get_prob_above(x_up)
+    #int_1 = dist_old.get_prob_between(x_up, x_down)
+    #int_2 = dist.get_prob_between(x_up, x_down)
+    #int_3 = dist_old.get_prob_below(x_up)
+    #int_4 = dist.get_prob_below(x_up)
+#    print(dist.get_prob_above(x_up) + (1 + eps_up) *\
+#            dist_old.get_prob_below(x_up))
+#    print(dist.get_prob_below(x_down) + (1 - eps_down) *\
+#            dist_old.get_prob_above(x_down))
+#    print(-eps_down - ((1 - eps_down) * (dist_old.get_prob_between(x_up,\
+#        x_down) + dist_old.get_prob_below(x_up))) \
+#        + dist.get_prob_between(x_up, x_down) + dist.get_prob_below(x_up))
 
     discrepancy = eps_down + ((1 - eps_down) * int_1) - \
                 (int_2 + ((eps_up + eps_down) * int_3))
 
     penalty_contributions = \
         (2 * int_4) - ((2 + eps_up - eps_down)* int_3) - eps_down - \
-        ((1 - eps_down) * int_1) + dist.get_prob_between(x_down, x_up)
+        ((1 - eps_down) * int_1) + int_2
 
     return discrepancy, penalty_contributions
 
@@ -55,8 +68,11 @@ class NormalUtil(normal.Normal):
 learning_rate = 0.01
 
 std = torch.tensor([1.0])
+mean_old_orig = torch.tensor([0.1])
+mean_orig = torch.tensor([-0.4])
+
 mean_old = torch.tensor([0.0])
-mean = torch.tensor([0.5])
+mean = torch.abs(mean_orig - mean_old_orig)
 
 dist_old = NormalUtil(mean_old, std)
 dist = NormalUtil(mean, std)

ppo_torch/ppo_model.py
@@ -11,7 +11,7 @@ Encapsulates relevant PPO parameters and procedures.
 """
 class PPOModel(object):
     def __init__(self, env, num_hidden_layers, hidden_layer_size, alpha, \
-        clip_param):
+        clip_param_up, clip_param_down):
         # - set up networks -
         self.policy_net = \
             PolicyNetVar(env, num_hidden_layers, hidden_layer_size, False,
@@ -32,7 +32,8 @@ class PPOModel(object):
             GeneralNet(env, num_hidden_layers, hidden_layer_size, 1).to("cpu")
         # - 
 
-        self.clip_param = clip_param
+        self.clip_param_up = clip_param_up
+        self.clip_param_down = clip_param_down
 
         # set up optimizer
         self.optimizer = optim.Adam(self.trainable_parameters(), alpha)
@@ -110,6 +111,13 @@ class PPOModel(object):
             total_mag_sq += diffmag
 
         return total_mag_sq
+
+    """
+    Returns clip params, adjusted to minimize expected penalty contribution
+    discrepancy between positive and negative estimators, while maintaining the
+    same total expected penalty contributions. Uses absolute average distance
+    from the mean as 
+    """
         
     """
     Calculates the total loss with the given batch data.
@@ -122,8 +130,8 @@ class PPOModel(object):
         
         # - calculate policy loss -
         surr1 = ratio * target
-        surr2 = torch.clamp(ratio, min=1.0 - self.clip_param, max=1.0 +\
-                self.clip_param) * target
+        surr2 = torch.clamp(ratio, min=1.0 - self.clip_param_down, max=1.0 +\
+                self.clip_param_up) * target
         pol_loss = -torch.mean(torch.min(surr1, surr2))
         # - 
 

ppo_torch/run.py
@@ -36,7 +36,8 @@ g_seed = 0
 # -- 
 
 # epsilon as described by Schulman et. al.
-g_clip_param = 0.2
+g_clip_param_up = 0.2
+g_clip_param_down = 0.2
 
 # -- SGD parameters --
 # number of training epochs per run
@@ -63,8 +64,8 @@ g_env_name = "InvertedPendulum-v2"
 Trains a PPO agent according to given parameters and reports results.
 """
 def train(hidden_layer_size, num_hidden_layers, num_timesteps, \
-    timesteps_per_rollout, seed, clip_param, num_epochs, alpha, \
-    batch_size, gamma, lambda_, env_name):
+    timesteps_per_rollout, seed, clip_param_up, clip_param_down, num_epochs, \
+    alpha, batch_size, gamma, lambda_, env_name):
     # - setup -
     # set up environment 
     env = gym.make(env_name)
@@ -75,7 +76,7 @@ def train(hidden_layer_size, num_hidden_layers, num_timesteps, \
 
     # create relevant PPO networks
     model = PPOModel(env, num_hidden_layers, hidden_layer_size, alpha,\
-            clip_param)
+            clip_param_up, clip_param_down)
 
     # total number of timesteps trained so far
     timesteps = 0
@@ -126,17 +127,21 @@ def train(hidden_layer_size, num_hidden_layers, num_timesteps, \
     
         # - gather graph data -
         ratio = model.ratio(rollout[RO_OB], rollout[RO_AC])
-        clip_param_tensor = torch.tensor(model.clip_param, device=device)
+        clip_param_up_opt, clip_param_down_opt = \
+            model.optimize_clip_params(rollout[RO_OB])
+        clip_param_up_tensor = torch.tensor(model.clip_param_up, device=device)
+        clip_param_down_tensor = \
+            torch.tensor(model.clip_param_down, device=device)
         avg_ratio_upclipped = torch.min(ratio, \
-            1 + clip_param_tensor).mean().item()
+            1 + clip_param_up_tensor).mean().item()
         avg_ratio_downclipped = torch.max(ratio, \
-            1 - clip_param_tensor).mean().item()
+            1 - clip_param_down_tensor).mean().item()
         avg_ratio = ratio.mean().item()
         avg_upclip_ded = avg_ratio - avg_ratio_upclipped
         avg_downclip_ded = avg_ratio_downclipped - avg_ratio
-        num_upclips = torch.sum(ratio > (1 + model.clip_param)).item()
-        num_downclips = torch.sum(ratio < (1 - model.clip_param)).item()
-        actual_clips = torch.sum(ratio > (1 + model.clip_param)).item()
+        num_upclips = torch.sum(ratio > (1 + clip_param_up_tensor)).item()
+        num_downclips = torch.sum(ratio < (1 - clip_param_down_tensor)).item()
+        actual_clips = torch.sum(ratio > (1 + clip_param_up_tensor)).item()
 
         graph_data[GD_AVG_NUM_UPCLIPS].append(num_upclips)
         graph_data[GD_AVG_NUM_DOWNCLIPS].append(num_downclips)
@@ -163,7 +168,8 @@ def global_run_seed(seed):
     return train(hidden_layer_size=g_hidden_layer_size, \
         num_hidden_layers=g_num_hidden_layers, num_timesteps=g_num_timesteps, \
         timesteps_per_rollout=g_timesteps_per_rollout, seed=seed, \
-        clip_param=g_clip_param, num_epochs=g_num_epochs, alpha=g_alpha, \
+        clip_param_up=g_clip_param_up, clip_param_down=g_clip_param_down, \
+        num_epochs=g_num_epochs, alpha=g_alpha, \
         batch_size=g_batch_size, gamma=g_gamma, lambda_=g_lambda_, \
         env_name=g_env_name)
 
@@ -184,7 +190,7 @@ def graph_ded_contr(data):
         color=(0.0, 0.0, 0.0), \
         label='$1 - E[r_{t, CLIP}^+]$')
     plt.plot(iterations, data[GD_AVG_DOWNCLIP_DED], linestyle='-', \
-        color=(0.0, 0.0, 0.0), \
+        color=(0.5, 0.5, 0.5), \
         label='$E[r_{t, CLIP}^-] - 1$')
 
     plt.legend()
@@ -243,7 +249,7 @@ def get_average_data(all_data):
     
 def data_run():
     all_data = []
-    for seed in range(3):
+    for seed in range(1):
         graph_data = global_run_seed(seed)
         all_data.append(graph_data)
 